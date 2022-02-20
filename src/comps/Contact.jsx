b8e4a2469025f5636ed04816cf7c76fcbf649941
Matt C <Cookem529@gmail.com>
2022-02-20 14:42:53 -0500

    Have social icons working
    Much better and maybe work on responsive sizing down the line

--- src/comps/Contact.jsx
+++ src/comps/Contact.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
 import Header from './Header'
 import ExternalLink from './ExternalLink'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 let socials = [
   {
     name: 'linkedin',
     url: 'https://www.linkedin.com/in/cookem529/',
     alt: 'External link to linkedin',
-    icon: 'temp',
+    icon: 'fa-brands fa-linkedin ',
   },
   {
     name: 'Discord',
     url: 'https://discordapp.com/users/Leafsicle#7877',
     alt: 'External link to Discord',
-    icon: 'temp',
+    icon: 'fa-brands fa-discord ',
   },
   {
     name: 'Github',
     url: 'https://github.com/leafsicle',
     alt: 'External link to GitHub',
-    icon: 'temp',
+    icon: 'fa-brands fa-github ',
   },
   {
     name: 'Twitter',
     url: 'https://twitter.com/M__cooke',
     alt: 'External link to Twitter',
-    icon: 'temp',
+    icon: 'fa-brands fa-twitter ',
   },
 ]
 const Contact = () => {
   return (
     <div className='mx-auto body bg-secondary '>
       <Header />
-      <main className='contrast'>
+      <main className=''>
         {socials.map((social, index) => (
           <ExternalLink socialInfo={social} key={index} />
         ))}
--- src/comps/ExternalLink.jsx
+++ src/comps/ExternalLink.jsx
@@ -4,10 +4,15 @@ const ExternalLink = ({ socialInfo }) => {
   return (
     <button
       role='button'
-      className='contrast mr-8 text-2xl text-white font-sans'
+      className='contrast text-2xl text-white font-sans'
       title={'External link to ' + socialInfo.name}>
       <a target='_blank' rel='noopener' href={socialInfo.url}>
-        {socialInfo.icon}
+        <i
+          className={
+            socialInfo.icon +
+            'bg-primary text-white rounded-md p-2 m-5 hover:bg-red hover:text-active '
+          }
+        />
       </a>
     </button>
   )
