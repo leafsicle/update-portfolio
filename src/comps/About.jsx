a9c58d87297f5b8e39112630fe1d6f6917ebaf2a
Matt C <Cookem529@gmail.com>
2022-02-20 14:45:33 -0500

    adjusting style

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -3,10 +3,17 @@ import Header from './Header'
 
 const About = () => {
   return (
-    <div className='body bg-secondary'>
+    <div className='body bg-secondary contrast text-white text-2xl font-sans'>
       <Header />
-
-      <main className='text-white text-2xl'></main>
+      <main className=' '>
+        <p>This is a story all about how my life got turned all upside down</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quia
+          voluptas, ipsum, ipsa est dolores inventore molestias perferendis
+          animi laboriosam exercitationem quae necessitatibus ex hic et
+          voluptates provident dolorum sint!
+        </p>
+      </main>
     </div>
   )
 }
--- src/comps/Contact.jsx
+++ src/comps/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './Header'
 import ExternalLink from './ExternalLink'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 let socials = [
   {
