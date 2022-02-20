e43d8a26a531d88fe9c058922e73f034936139cc
Matt C <Cookem529@gmail.com>
2022-02-20 00:13:44 -0500

    Other skeletons for the components

---
+++ src/comps/Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 bg-blue-500 min-h-screen'>
+      <div className='flex justify-between items-center border-b-2 border-indigo-100 py-6 md:justify-start md:space-x-10'>
+        <div className='flex justify-start lg:w-0 lg:flex-1 '>
+          <a
+            href='/'
+            className='text-indigo-200 hover:text-emerald-400 hover:rotate-90 '>
+            test
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Content
