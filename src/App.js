930acda29c7a22ec254490981c4b880ff1e9e8a5
Matt C <Cookem529@gmail.com>
2022-02-21 01:04:42 -0500

    making bio human-readable

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import image from './self.jpg'
 function App() {
   return (
     <div className='body bg-secondary contrast text-white lg:text-3xl text-2xl font-sans'>
-      <main className='object-center'>
+      <main className='mb-5'>
         <img
           src={image}
           alt='Image of Me. Matt Cooke.'
+          title='Did you come here looking for something fun after being trained for over a decade by XKCD? 🥚'
           className='w-1/5 pt-5 mx-auto rounded-full'
         />
       </main>
-      <aside className='text-active hover:text-white'>
-        Currently living in Baltimore Maryland
-      </aside>
+      <section className='text-active hover:text-red  mb-5'>
+        <a
+          target='_blank'
+          rel='noopener'
+          href='https://goo.gl/maps/HRPNGhGiPNufj7zA6'
+          alt='link to google map of Baltimore, MD'>
+          Currently living in Baltimore Maryland
+        </a>
+      </section>
 
       <Contact />
     </div>
--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='body bg-secondary contrast text-white lg:text-3xl text-xl font-sans'>
-      <main className=' '>
-        <p className='mb-4'>
-          incessantly curious I recently earned my Masters degree big picture
-          thinker responsive design clean code languages I earned a certificate
-          in web development from the Suncoast Developer’s Guild in St.
-          Petersburg, Florida in 2018 Prior to that, I was in the US Army and
-          was serving as an All-Source Intelligence Analyst. My academic
-          accomplishments demonstrate that I have an unbridled passion for the
-          work that I do.
-        </p>
-      </main>
-    </div>
+    <main className='body bg-secondary contrast text-white lg:text-3xl text-xl font-sans'>
+      <h5 className='pt-4 text-5xl'>About Me</h5>
+      <h3 className='pt-4 text-3xl'>I am incessantly curious.</h3>
+      <p className='mt-5 mx-9 hover:bg-black hover:rounded-2xl'>
+        I recently earned my Masters degree in Cybersecurity. I try to always
+        envision the larger picture. I favor accessible and responsive designs
+        that are cleanly written. responsive design clean code languages I
+        earned a certificate in web development from the Suncoast Developer’s
+        Guild in St. Petersburg, FL in 2018. Prior to that, I was in the US Army
+        and was serving as an All-Source Intelligence Analyst. My academic
+        accomplishments demonstrate that I have an unbridled passion for the
+        work that I do.
+      </p>
+    </main>
   )
 }
 
